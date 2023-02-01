03df9c680fac690735c8cf543384210217bdb3ed
Brandon <bpugh@stu.jsu.edu>
2023-01-31 22:09:01 -0600

    Completed Participation Assignment 2

---
+++ test/edu/jsu/mcis/JUnitTest.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package edu.jsu.mcis;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Brandon Pugh
+ */
+public class JUnitTest {
+    
+    public JUnitTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    
+    @Test
+    public void testGreeting(){
+        
+    }
+    
+    @Test
+    public void testReverseGreeting(){
+        
+    }
+    
+    @Test
+    public void testReverseMessage1() {
+        
+    }
+    
+        @Test
+    public void testReverseMessage2() {
+        
+    }
+    
+        @Test
+    public void testReverseMessage3() {
+        
+    }
+}
